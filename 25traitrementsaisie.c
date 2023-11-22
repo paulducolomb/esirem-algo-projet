9d8bad4f8608b82804683ccc0267cc7e17eb4f82
paulducolomb <144220018+paulducolomb@users.noreply.github.com>
2023-11-22 18:27:47 +0100

    ajout du while pour etre sur que le valeurs sont bien valides

--- 25traitrementsaisie.c
+++ 25traitrementsaisie.c
@@ -3,4 +3,9 @@ PlayerChoice traiter_saisie_utilisateur() {
     printf("Entrez votre choix (0: HIT, 1: STAND, 2: DOUBLE, 3: SURRENDER): \n");
     scanf("%d", &choix);
 
+    while (choix < 0 || choix > 3) {
+        printf("Choix invalide, Réésayez : \n");
+        scanf("%d", &choix);
+    }
+    return (PlayerChoice)choix;
 }
