7a7e09488f9298813981645f8f9b49273a4005a0
paulducolomb <144220018+paulducolomb@users.noreply.github.com>
2023-11-22 17:47:20 +0100

    fonction à jour

--- 23affichagemain.c
+++ 23affichagemain.c
@@ -1,1 +1,6 @@
-test
+void afficher_main(Main main) {
+    printf("Main de cartes:\n");
+    for (int i = 0; i < main.nombre_cartes; i++) {
+        printf("Carte %d:  %d de %d, \n", i + 1, main.cartes[i].valeur, main.cartes[i].couleur);
+    }
+}
