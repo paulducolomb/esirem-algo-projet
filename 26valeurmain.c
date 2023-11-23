203697a5801ac3e29d38e1628096492567315249
paulducolomb <144220018+paulducolomb@users.noreply.github.com>
2023-11-23 09:56:14 +0100

    mise a jour

---
+++ 26valeurmain.c
@@ -0,0 +1,13 @@
+int calculer_valeur_main(struct joueur joueur) {
+    int valeur_totale = 0;
+    struct deck* courant = joueur.cartes_en_main;
+    while (courant != NULL) {
+        int valeur_carte = courant->Card.valeur;
+        if (valeur_carte > 10) { // Pour les figures (Valet, Dame, Roi)
+            valeur_carte = 10;
+        }
+        valeur_totale += valeur_carte;
+        courant = courant->next;
+    }
+    return valeur_totale;
+}
