dd793c28b21292ab9e0b7150c6d73ee5fc8f88c5
Théophile REY <83590565+TheophileREY@users.noreply.github.com>
2023-11-23 09:34:59 +0100

    Finalisation deckjoueur.c

--- deckjoueur.c
+++ deckjoueur.c
@@ -2,33 +2,50 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Fonction pour initialiser un deck de cartes
-void initialiser_deck(ListeCartes *deck) {
-    // Allocation de mémoire pour un tableau de 52 cartes dans le deck
-    deck->cartes = malloc(sizeof(Carte) * 52);
-    deck->taille = 52; // Taille du deck = 52 cartes
-    int index = 0;
-    // Boucles pour assigner chaque valeur de carte (de 1 à 13) à chaque couleur de carte (4 couleurs)
-    for (int val = 1; val <= 13; val++) {
-        for (int coul = Diamond; coul <= Spade; coul++) {
-            deck->cartes[index].valeur = Valeur; // Assignation de la valeur de la carte
-            deck->cartes[index].couleur = Couleur; // Assignation de la couleur de la carte
-            index++;
+void initialiser_deck(struct deck **sommet) {
+    *sommet = NULL; // Initialise le sommet du deck à NULL
+    struct deck *precedent = NULL; // Initialise un pointeur precedent à NULL
+    for (int coul = Heart; coul <= Spade; coul++) { // Boucle sur les couleurs de Heart à Spade
+        for (int val = 1; val <= 13; val++) { // Boucle sur les valeurs de 1 à 13
+            struct deck *nouvelleCarte = (struct deck*)malloc(sizeof(struct deck)); // Allouer de la mémoire pour une nouvelle carte
+            nouvelleCarte->Card.valeur = val; // Assigne la valeur de la carte
+            nouvelleCarte->Card.Couleur = coul; // Assigne la couleur de la carte
+            nouvelleCarte->next = NULL; // Initialise le pointeur 'next' de la nouvelle carte à NULL
+            if (*sommet == NULL) { // Vérifie si le sommet du deck est vide
+                *sommet = nouvelleCarte; // Si oui, la nouvelle carte devient le sommet du deck
+            } else {
+                precedent->next = nouvelleCarte; // Sinon, la carte précédente pointe vers la nouvelle carte
+            }
+            precedent = nouvelleCarte; // La nouvelle carte devient la carte précédente pour la prochaine itération
         }
     }
 }
 
 // Fonction pour mélanger un deck de cartes
-void melanger_deck(ListeCartes *deck) {
-    srand(time(NULL)); // Réinitialisation de rand()
-
-    // Mélange les cartes dans le deck
-    for (size_t i = deck->taille - 1; i > 0; i--) {
-        size_t j = rand() % (i + 1);
-
-        // Échange des cartes aux positions i et j dans le deck
-        Carte temp = deck->cartes[i];
-        deck->cartes[i] = deck->cartes[j];
-        deck->cartes[j] = temp;
+void melanger_deck(struct deck **sommet) {
+    int taille = 0; // Initialise taille a 0 pour stocker la taille du deck
+    struct deck *courant = *sommet; // Initialise un pointeur courant au sommet du deck
+    // Calcule la taille du deck en parcourant tous les éléments du deck
+    while (courant != NULL) {
+        taille++; // Incrémente la taille à chaque itération
+        courant = courant->next; // Déplace le pointeur courant vers l'élément suivant du deck
+    }
+    // Vérifie si la taille du deck est supérieure à 1 pour effectuer un mélange
+    if (taille > 1) {
+        srand(time(NULL)); // Initialise le générateur de nombres aléatoires
+        // Boucle pour mélanger le deck
+        for (int i = 0; i < taille - 1; i++) {
+            int j = i + rand() % (taille - i); // Génère un nombre aléatoire 'j' dans la plage des cartes restantes
+            // Initialisation de deux pointeurs 'courant_i' et 'courant_j' pour les cartes à échanger
+            struct deck *courant_i = *sommet;
+            struct deck *courant_j = *sommet;
+            // Déplace les pointeurs 'courant_i' et 'courant_j' vers les cartes à échanger
+            for (int k = 0; k < i; k++) courant_i = courant_i->next;
+            for (int k = 0; k < j; k++) courant_j = courant_j->next;
+            // Échange les valeurs des cartes 'courant_i' et 'courant_j'
+            struct cartes temp = courant_i->Card;
+            courant_i->Card = courant_j->Card;
+            courant_j->Card = temp;
+        }
     }
 }
